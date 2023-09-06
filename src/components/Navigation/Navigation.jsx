1c50942eb07f70beee8cd9c5b2c9a5c48a07d6d0
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-09-06 08:42:00 +0200

    feat(navigation): add button contacts to the homepage whne logged in

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,16 +2,23 @@ import { NavLink } from 'react-router-dom';
 import { Box, Button } from '@mui/material';
 
 import React from 'react';
+import { useLog } from 'hooks/useLog';
 
 export const Navigation = () => {
+  const { isLoggedIn } = useLog();
   return (
     <Box
       component="nav"
-      sx={{ flexGrow: 1, flexWrap: 'wrap', display: { xs: 'flex' } }}
+      sx={{ flexGrow: 1, gap: 1, flexWrap: 'wrap', display: { xs: 'flex' } }}
     >
       <NavLink to="/">
         <Button variant="contained">Home</Button>
       </NavLink>
+      {isLoggedIn && (
+        <NavLink to="/contacts">
+          <Button variant="contained">Contacts</Button>
+        </NavLink>
+      )}
     </Box>
   );
 };
